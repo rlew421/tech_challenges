454af56f1e289a4487b38d363d849b01d612f702
Rachel Lew <rlew07@gmail.com>
2020-10-20 23:50:56 -0500

    Create separate directories by language

---
+++ javascript/two_sum.js
@@ -0,0 +1,14 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//
+// You can return the answer in any order.
+
+var twoSum = function(numsArray, targetInteger) {
+  // Create a new object where the keys are the original nums and the values are the difference between each number and the target integer
+  // Create an empty object
+  let numberIndex = {};
+  let result = [];
+
+  // Iterate over each number and its index in the numbers array
+};
